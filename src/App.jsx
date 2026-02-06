940dcfc177419699b28c1c00755015d8fce982c9
nique-e <niteshmule023@gmail.com>
2026-02-06 16:36:25 +0530

    this is react folder

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Card from './components/card'
+
+const App = () => {
+
+  const user = "Nitesh";
+  const age = 68;
+  return (
+    <>
+  
+       <Card/>
+       <Card/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div className='parent'> 
+      <div className="card">
+        <img src="https://images.unsplash.com/photo-1768824264646-ea641cc71f5e?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        <h1>Nitesh Mule</h1> 
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum modi quam velit!</p>   
+        <button>view profile</button>
+    </div>
+    </div>
+  )
+}
+
+export default card
